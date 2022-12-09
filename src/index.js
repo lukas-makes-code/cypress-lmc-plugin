b7b91fefda4ba2f4b11cba1ae50a8d93ab65fc80
Lukas Maxheim <l.maxheim@forty-four.de>
2022-12-09 18:59:52 +0100

    [TASK] Add basic command for validating imprint links

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { acceptCookieBot } from './commands/accept-cookiebot';
 import { authorizeAgainstHtaccess } from './commands/authorize-against-htaccess';
+import { validateImprintLinkIsClickable } from './commands/validate-imprint-is-clickable';
 
 Cypress.Commands.add('acceptCookieBot', acceptCookieBot);
 Cypress.Commands.add('authorizeAgainstHtaccess', authorizeAgainstHtaccess);
+Cypress.Commands.add('validateImprintIsClickable', validateImprintLinkIsClickable);
