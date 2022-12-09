17bfba6293e10e561de73a8e249ec4534c9a670d
Lukas Maxheim <guten-tag@lukas-mx.de>
2022-12-09 23:41:14 +0100

    [BUGFIX] Fix import path for validate-imprint-link-is-clickable-function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { acceptCookieBot } from './commands/accept-cookiebot';
 import { authorizeAgainstHtaccess } from './commands/authorize-against-htaccess';
-import { validateImprintLinkIsClickable } from './commands/validate-imprint-is-clickable';
+import { validateImprintLinkIsClickable } from './commands/validate-imprint-link-is-clickable';
 
 Cypress.Commands.add('acceptCookieBot', acceptCookieBot);
 Cypress.Commands.add('authorizeAgainstHtaccess', authorizeAgainstHtaccess);
